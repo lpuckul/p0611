5221e2d7adcda7a258f32ced3be4b85ec7f4058f
lpuckul <lpuckul@naver.com>
2018-06-11 17:55:55 +0900

    commit message 2

--- git_project/src/git_project/hellowolrd.java
+++ git_project/src/git_project/hellowolrd.java
@@ -2,7 +2,7 @@ package git_project;
 
 public class hellowolrd {
 	public static void main(String[] args) {
-	System.out.println("helloworld");
+	System.out.println("helloworld Çï·Î¿ì¿ùµå");
 
 }
 }
